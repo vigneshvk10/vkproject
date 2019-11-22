28516be26f1201976b35f12f04e17ec782a77c25
unknown <vigneshforu10@gmail.com>
2019-11-22 14:18:45 +0530

    vkproject

---
+++ src/green.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class green {
+public static void main(String[] args) {
+	Set<Integer> s=new HashSet<>();
+	s.add(10);
+	s.add(50);
+	s.add(30);
+	System.out.println(s);
+	for (Integer g : s) {
+	System.out.println(g);	
+		
+	}
+	
+}
+}
